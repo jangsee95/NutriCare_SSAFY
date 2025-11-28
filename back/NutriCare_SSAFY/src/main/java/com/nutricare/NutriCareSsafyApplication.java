3f0c157d08a65b48fc863c75c64d271233904df2
jangjoonho <jangsee95@icloud.com>
2025-11-28 16:12:34 +0900

    update : GcsProperties 클래스 설정추가

--- back/NutriCare_SSAFY/src/main/java/com/nutricare/NutriCareSsafyApplication.java
+++ back/NutriCare_SSAFY/src/main/java/com/nutricare/NutriCareSsafyApplication.java
@@ -2,8 +2,11 @@ package com.nutricare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.nutricare.config.GcsProperties;
 @SpringBootApplication
+@EnableConfigurationProperties({GcsProperties.class})
 public class NutriCareSsafyApplication {
 
 	public static void main(String[] args) {
